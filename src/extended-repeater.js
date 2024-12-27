704b6cacee11a6c94116e9c473c72ab19668206a
Yana Malakhova <ya6ka0@gmail.com>
2024-12-28 02:34:20 +0300

    feat: write extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const anyStr = '' + str;
+  const repeatTimes = options.repeatTimes ?? 1;
+  const stringSeparator = options.separator ?? '+';
+  const stringAddition = (options.addition || options.addition === null || options.addition === false)
+    ? '' + options.addition
+    : '';
+  const additionRepeatTimes = options.additionRepeatTimes ?? 1;
+  const additionSeparator = options.additionSeparator ?? '|';
+
+  const additionString = (additionSeparator.length === 0)
+    ? `${stringAddition}${additionSeparator}`
+      .repeat(additionRepeatTimes)
+      .slice(0)
+    : `${stringAddition}${additionSeparator}`
+      .repeat(additionRepeatTimes)
+      .slice(0, -additionSeparator.length);
+
+  const result = (stringSeparator.length === 0)
+    ? `${anyStr}${additionString}${stringSeparator}`
+      .repeat(repeatTimes)
+      .slice(0)
+    : `${anyStr}${additionString}${stringSeparator}`
+      .repeat(repeatTimes)
+      .slice(0, -stringSeparator.length);
+
+  return result;
 }
 
 module.exports = {
