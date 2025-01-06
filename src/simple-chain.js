725793681f36e4379d2039a06229083e457c9296
Yana Malakhova <ya6ka0@gmail.com>
2025-01-06 04:00:29 +0300

    feat: write simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    const newChainElement = value || value === null || value === 0 || value === false || Number.isNaN(value)
+    ? `( ${value} )`
+    : '(  )';
+
+    this.chain.push(newChainElement);
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position <= 0 || position > this.chain.length || !Number.isInteger(position)) {
+      this.chain = [];
+      throw new Error("You can\'t remove incorrect link!");
+    }
+
+    this.chain.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const resultChain = this.chain.join('~~');
+    this.chain = [];
+
+    return resultChain;
   }
 };
 
